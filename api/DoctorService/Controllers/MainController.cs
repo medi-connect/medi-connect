78cf0fd0ec29082d77b9d6c8e2a147470158776c
srebrovn <srebrov4@gmail.com>
2024-11-14 16:09:59 +0100

    Added connection to DB, new api points for reading/writing doctor users.

--- api/DoctorService/Controllers/MainController.cs
+++ api/DoctorService/Controllers/MainController.cs
@@ -1,21 +1,87 @@
 using DoctorService.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace DoctorService.Controllers;
 
 
 [ApiController]
 [Route("/api/v1/[controller]")]
-public class MainController
+public class MainController: ControllerBase
 {
+    
+    private readonly string _connectionString;
+    private readonly SqlConnection connection; 
+    public MainController(IConfiguration configuration)
+    {
+        _connectionString = configuration["DB_CONNECTION_STRING"] ?? "";
+        connection = new SqlConnection(_connectionString);
+        connection.Open();
+    }
+    
     [HttpGet("getDoctor")]
     public DoctorModel GetDoctor()
     {
         return new DoctorModel
         {
-            Name = "Doctor 1",
-            Surname = "Doctor 2",
-            HospitalName = "Hospital1"
+            FirstName = "Doctor 1",
+            LastName = "Doctor 2",
+            Speciality = "Hospital1"
         };
+    }
+    
+    [HttpGet("getDoctor/{id}")]
+    public List<DoctorModel> GetDoctor([FromRoute] string id)
+    {
+        var rows = new List<DoctorModel>();
+        var command = new SqlCommand("SELECT * FROM Doctor WHERE id = @id", connection);
+        command.Parameters.AddWithValue("@id", id);
+        using SqlDataReader reader = command.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                rows.Add(new DoctorModel
+                {
+                    FirstName = reader.GetString(1),
+                    LastName = reader.GetString(2),
+                    Email = reader.GetString(3),
+                    Speciality = reader.GetString(4),
+                    BirthDate = reader.GetDateTime(5),
+                }); 
+            }
+        }
+
+        return rows;
+    }
+
+    [HttpPost("addDoctor")]
+    public async Task<ActionResult> AddDoctor([FromBody] DoctorModel doctor)
+    {
+        try
+        {
+            if (doctor == null)
+            {
+                return BadRequest("Doctor information is incomplete.");
+            }
+            
+            var command = new SqlCommand(
+                "INSERT INTO Doctor (FirstName, LastName, Email, Speciality, BirthDate) OUTPUT INSERTED.id VALUES (@firstName, @lastName, @email, @speciality, @birthDate)", connection);
+            command.Parameters.AddWithValue("@firstName", doctor.FirstName);
+            command.Parameters.AddWithValue("@lastName", doctor.LastName);
+            command.Parameters.AddWithValue("@email", doctor.Email);
+            command.Parameters.AddWithValue("@speciality", doctor.Speciality);
+            command.Parameters.AddWithValue("@birthDate", doctor.BirthDate);
+                
+            var newId = (int)(await command.ExecuteScalarAsync() ?? 0);
+            return newId > 0 ? CreatedAtAction(nameof(GetDoctor), new { id = newId }, doctor) : StatusCode(500, "Error while inserting.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        } 
+        
+
     }
 }
