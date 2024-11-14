31e0d95f9ad813ff3ed078e138b1959511f3a8a4
srebrovn <srebrov4@gmail.com>
2024-11-14 16:07:59 +0100

    Added reading ENV values from .env in Program.cs

--- api/DoctorService/Program.cs
+++ api/DoctorService/Program.cs
@@ -1,8 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
+using DotNetEnv;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+Env.Load();
 
 builder.Services.AddControllers();
+builder.Configuration.AddEnvironmentVariables();
 
 var app = builder.Build();
 
