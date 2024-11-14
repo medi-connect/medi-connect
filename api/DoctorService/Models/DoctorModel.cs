d259fda3786c54e8040b53524d2848a592c54107
srebrovn <srebrov4@gmail.com>
2024-11-14 16:09:26 +0100

    Updated DoctorModel.

--- api/DoctorService/Models/DoctorModel.cs
+++ api/DoctorService/Models/DoctorModel.cs
@@ -2,7 +2,9 @@ namespace DoctorService.Models;
 
 public class DoctorModel
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string HospitalName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Speciality { get; set; }
+    public DateTime BirthDate { get; set; }
 }
