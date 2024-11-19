9d232e5f549302a9e14ac973a40cf927241e96b0
srebrovn <srebrov4@gmail.com>
2024-11-19 16:56:26 +0100

    Added DB Connection and new dependencies to UserService.

--- api/UserService/Program.cs
+++ api/UserService/Program.cs
@@ -1,8 +1,15 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using UserService.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+Env.Load();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")));
 
 var app = builder.Build();
 
