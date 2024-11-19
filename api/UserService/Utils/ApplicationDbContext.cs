fd639df951133973011aef13364fa943436cc553
srebrovn <srebrov4@gmail.com>
2024-11-19 16:56:49 +0100

    Added APP Db Context.

---
+++ api/UserService/Utils/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+
+namespace UserService.Controllers;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<UserDTO> UserAccount { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserDTO>(entity =>
+        {
+            // Specify the table name and schema here
+            entity.ToTable("UserAccount", "dbo");
+
+            entity.HasKey(e => e.Id);
+            // Map properties to columns, specifying snake_case names
+            entity.Property(e => e.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            entity.Property(e => e.Email).HasColumnName("email");
+            entity.Property(e => e.Password).HasColumnName("password");
+            entity.Property(e => e.Status).HasColumnName("status");
+        });
+    }
+}
