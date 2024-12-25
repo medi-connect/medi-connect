caf82ea8b1b6330e4558ed673ad89032e508bb01
srebrovn <srebrov4@gmail.com>
2024-12-25 10:15:52 +0100

    Updated EF in UserService

--- api/UserService/Utils/ApplicationDbContext.cs
+++ api/UserService/Utils/ApplicationDbContext.cs
@@ -5,23 +5,29 @@ namespace UserService.Controllers;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
-    public DbSet<UserModel> UserAccount { get; set; }
-    
+    public DbSet<UserModel> UserAccount { get; set; }    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<UserModel>(entity =>
         {
-            // Specify the table name and schema here
-            entity.ToTable("UserAccount", "dbo");
-
-            entity.HasKey(e => e.Id);
-            // Map properties to columns, specifying snake_case names
-            entity.Property(e => e.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            entity.ToTable("UserAccount", "dbo"); // Base table
+            entity.HasKey(e => e.UserId);
+            entity.Property(e => e.UserId).HasColumnName("user_id").ValueGeneratedOnAdd();
             entity.Property(e => e.Email).HasColumnName("email");
             entity.Property(e => e.Password).HasColumnName("password");
             entity.Property(e => e.Status).HasColumnName("status");
         });
+
+        modelBuilder.Entity<PatientModel>(entity =>
+        {
+            entity.ToTable("Patient", "dbo"); // Derived table
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Surname).HasColumnName("surname");
+            entity.Property(e => e.BirthDate).HasColumnName("birth_date");
+
+        });
     }
+
 }
