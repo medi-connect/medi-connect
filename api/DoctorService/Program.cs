6a5a354c7e3c826b46b4ede156360e70256563b5
srebrovn <srebrov4@gmail.com>
2024-12-25 10:17:08 +0100

    Added DbContext in doctor service.

--- api/DoctorService/Program.cs
+++ api/DoctorService/Program.cs
@@ -1,10 +1,16 @@
+using DoctorService.Utils;
 using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 Env.Load();
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")));
+builder.Services.AddHttpClient();
+
 builder.Configuration.AddEnvironmentVariables();
 
 var app = builder.Build();
