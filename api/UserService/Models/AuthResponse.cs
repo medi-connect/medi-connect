797d6246d8ae40c717bfb4fd377677ccbb2c69b6
srebrovn <srebrov4@gmail.com>
2025-01-08 19:47:04 +0100

    Added checking if user is Doctor on login, and setting status on registration.

--- api/UserService/Models/AuthResponse.cs
+++ api/UserService/Models/AuthResponse.cs
@@ -5,4 +5,5 @@ public class AuthResponse
     public string Token { get; set; }
     public DateTime Expiration { get; set; }
     public string UserId { get; set; }
+    public bool IsDoctor { get; set; }
 }
