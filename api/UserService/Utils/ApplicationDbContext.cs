a2d87195a0dd82d48d338e12c6e5f884b9e2ebb7
srebrovn <srebrov4@gmail.com>
2025-01-08 18:03:06 +0100

    Changes

--- api/UserService/Utils/ApplicationDbContext.cs
+++ api/UserService/Utils/ApplicationDbContext.cs
@@ -19,15 +19,6 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             entity.Property(e => e.Password).HasColumnName("password");
             entity.Property(e => e.Status).HasColumnName("status");
         });
-
-        modelBuilder.Entity<PatientModel>(entity =>
-        {
-            entity.ToTable("Patient", "dbo"); // Derived table
-            entity.Property(e => e.Name).HasColumnName("name");
-            entity.Property(e => e.Surname).HasColumnName("surname");
-            entity.Property(e => e.BirthDate).HasColumnName("birth_date");
-
-        });
     }
 
 }
