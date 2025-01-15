6358ad9eb31dbaedb30b0c5b5df8cb65f2d423b3
srebrovn <srebrov4@gmail.com>
2025-01-15 10:09:28 +0100

    Added documentation, code refactor.

--- api/UserService/Controllers/UserController.cs
+++ api/UserService/Controllers/UserController.cs
@@ -14,8 +14,15 @@ public class UserController : ControllerBase
         this.userService = userService;
     }
 
+    /// <summary>
+    /// Process login request.
+    /// </summary>
+    /// <param name="request">Login request.</param>
+    /// <returns>The created user.</returns>
+    /// <response code="200">Returns JWT token</response>
+    /// <response code="400">If error occured or required fields are null</response>
     [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    public async Task<ActionResult> Login([FromBody] LoginRequest request)
     {
         if (!userService.UserExists(request.Email))
         {
@@ -25,7 +32,34 @@ public class UserController : ControllerBase
         return await LoginUser(request);
     }
     
-    private async Task<IActionResult> LoginUser(LoginRequest request)
+    /// <summary>
+    /// Registers a doctor.
+    /// </summary>
+    /// <param name="doctor">The doctor object to be created.</param>
+    /// <returns>The created doctor.</returns>
+    /// <response code="200">Returns the newly created doctor</response>
+    /// <response code="400">If the doctor already exists</response>
+    /// <response code="500">If error occured</response>
+    [HttpPost("registerDoctor")]
+    public async Task<ActionResult> RegisterDoctor([FromBody] DoctorModel doctor)
+    {
+        
+        if (userService.UserExists(doctor.Email))
+        {
+            return BadRequest("User already exists.");
+        }
+
+        doctor.UserId = await userService.RegisterUserInternal(doctor, true);
+
+        if (doctor.UserId is -1 or null)
+        {
+            return StatusCode(500, "An unexpected error occurred. Please try again later.");
+        }
+
+        return Ok(doctor);
+    }
+
+    private async Task<ActionResult> LoginUser(LoginRequest request)
     {
         try
         {
@@ -50,24 +84,6 @@ public class UserController : ControllerBase
         }
     }
     
-    [HttpPost("registerDoctor")]
-    public async Task<IActionResult> RegisterDoctor([FromBody] DoctorModel doctorModel)
-    {
-        
-        if (userService.UserExists(doctorModel.Email))
-        {
-            return BadRequest("User already exists.");
-        }
-
-        doctorModel.UserId = await userService.RegisterUserInternal(doctorModel, true);
-
-        if (doctorModel.UserId is -1 or null)
-        {
-            return StatusCode(500, "An unexpected error occurred. Please try again later.");
-        }
-
-        return Ok(doctorModel);
-    }
     public bool UserExistsStatic(string email)
     {
         return userService.UserExists(email);
